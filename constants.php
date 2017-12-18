3dcd5cca684cea85fd98720c79c74cbdc5a80765
bochkovyi <bochkovyi@gmail.com>
2017-12-18 10:42:43 +0200

    Update constants.php

--- constants.php
+++ constants.php
@@ -1,4 +1,5 @@
 <?php
 define('APP_LOCATION', __DIR__ . DIRECTORY_SEPARATOR);
 define('APP_TITLE', 'Simple App');
-define('BASE_HREF', '/');
+//define('BASE_HREF', '/');
+define('BASE_HREF', '/Simple-PHP-App/');
